6387b69dfdcbe9506bd1e06ee2132443d1aa3934
Sri Charith Rampelli <charithrampelli2005@gmail.com>
2025-09-23 22:30:47 +0530

    Add existing Zoom project files

---
+++ backend/src/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("PORT", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const start = async () => {
+    try {
+        const connectionDb = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MONGO Connected: ${connectionDb.connection.host}`);
+        console.log("Connected to DB:", connectionDb.connection.name);
+        
+        server.listen(process.env.PORT, () => {
+            console.log(`Server listening on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+
+
+
+
+start();
